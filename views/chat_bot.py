1468ad7c6d6bfda421403760cb32fc894468ff89
Emiliano Sanchez <128442463+ealss101@users.noreply.github.com>
2025-02-03 15:28:02 -0600

    Update chat_bot.py

--- views/chat_bot.py
+++ views/chat_bot.py
@@ -6,28 +6,46 @@ import os
 # Load environment variables
 load_dotenv()
 
-APPLICATION_TOKEN = os.getenv("APP_TOKEN")
+# streamlit auth
+APPLICATION_TOKEN = st.secrets["APP_TOKEN_SIMPLE"]
 BASE_API_URL = "https://api.langflow.astra.datastax.com"
 LANGFLOW_ID = "9013a235-fb25-4adc-9095-e285cb8cee04"
-FLOW_ID = "953f00a8-8e75-4ab6-8eab-5f058e7a9496"
-ENDPOINT = "galgo-ai-simple" # The endpoint name of the flow
+ENDPOINT = "galgo-ai-simple"
+
+# local auth
+# APPLICATION_TOKEN = os.getenv("APP_TOKEN_SIMPLE")
+# BASE_API_URL = "https://api.langflow.astra.datastax.com"
+# LANGFLOW_ID = "9013a235-fb25-4adc-9095-e285cb8cee04"
+# ENDPOINT = "galgo-ai-simple"
+
+# docker auth
+# BASE_API_URL = "http://localhost:7860"
+# FLOW_ID = "844b3bb7-af1c-442c-b233-e61d4850e6d4"
+# ENDPOINT = "galgo-ai-simple" # The endpoint name of the flow
 
 # Initialize session state
 if "logged_in" not in st.session_state or not st.session_state.get("logged_in", False):
     st.warning("Please log in to use the Chat Bot.")
 else:
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    if "messages_simple" not in st.session_state:
+        st.session_state.messages_simple = []
 
-    def run_flow(message: str) -> str:
+
+    def run_flow_simple(message: str) -> str:
         """
         Sends the user message to the LangFlow API and retrieves the response.
         """
+        # normal auth
         api_url = f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{ENDPOINT}"
         headers = {
             "Authorization": f"Bearer {APPLICATION_TOKEN}",
             "Content-Type": "application/json",
         }
+
+        # docker auth
+        # api_url = f"{BASE_API_URL}/api/v1/run/{ENDPOINT}"
+        # headers = None
+        
         payload = {
             "input_value": message,
             "output_type": "chat",
@@ -38,14 +56,14 @@ else:
         return response.json()["outputs"][0]["outputs"][0]["results"]["text"]["text"]
 
     # Display chat messages
-    for message in st.session_state.messages:
+    for message in st.session_state.messages_simple:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
     # User input
     if user_input := st.chat_input("Type your message here..."):
         # Add user input to chat history
-        st.session_state.messages.append({"role": "user", "content": user_input})
+        st.session_state.messages_simple.append({"role": "user", "content": user_input})
         with st.chat_message("user"):
             st.markdown(user_input)
 
@@ -54,7 +72,7 @@ else:
             placeholder = st.empty()  # Placeholder for the assistant's response
             try:
                 with st.spinner("Galgo AI is typing..."):
-                    response = run_flow(user_input)
+                    response = run_flow_simple(user_input)
                 placeholder.markdown(response)  # Replace placeholder with response
             except Exception as e:
                 placeholder.markdown("There was an error processing your request.")
@@ -62,4 +80,4 @@ else:
                 response = "There was an error processing your request."
 
         # Add assistant's response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages_simple.append({"role": "assistant", "content": response})
