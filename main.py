53310eaa49b5f56788ae1355c9c904f579b3e2c2
Emiliano Sanchez <128442463+ealss101@users.noreply.github.com>
2025-02-08 11:51:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ about_us_page = st.Page(
 )
 chat_bot_page = st.Page(
     "views/chat_bot.py",
-    title="Galgo AI",
+    title="𝐀ll 𝐈n",
     icon=":material/robot:",
 )
 accounting_research_chat_bot_page = st.Page(
     "views/accounting_research_chat_bot.py",
-    title="Galgo [Accounting Research]",
+    title="Accounting Researcher",
     icon=":material/psychology:",
+)
+law_agent_chat_bot_page = st.Page(
+    "views/law_agent_chat_bot.py",
+    title="Lawyer's Agent",
+    icon=":material/gavel:",
 )
 settings_page = st.Page(
     "views/settings.py",
@@ -57,10 +62,25 @@ profile_page = st.Page(
 pg = st.navigation(
     {
         "Account": [login_page, profile_page, settings_page, terms_page],
-        "Features": [chat_bot_page, accounting_research_chat_bot_page, analytics_page],
+        "Galgo AI Features": [chat_bot_page, accounting_research_chat_bot_page, law_agent_chat_bot_page, analytics_page],
         "Info": [about_us_page, instructions_page, support_page],
     }
 )
+
+
+with st.sidebar.expander("**Start a New Chat**", expanded=False):
+    
+    if st.button("All In", key="new_all_sidebar"):
+        st.session_state.messages_simple = []
+        st.switch_page("views/chat_bot.py")
+
+    if st.button("Lawyer's Agent", key="new_law_sidebar"):
+        st.session_state.messages_law = []
+        st.switch_page("views/law_agent_chat_bot.py")
+
+    if st.button("Accounting Researcher", key="new_research_sidebar"):
+        st.session_state.messages_research = []
+        st.switch_page("views/accounting_research_chat_bot.py")
 
 # --- RUN NAVIGATION ---
 pg.run()
